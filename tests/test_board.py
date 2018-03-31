292a956ee494dc67bedd066440fec6550a96dcc9
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2018-03-31 19:53:52 +0200

    TST: Add a unit test for the `from_str` class method

--- tests/test_board.py
+++ tests/test_board.py
@@ -3,5 +3,13 @@ import unittest
 from ..board import Board
 
 class TestBoard(unittest.TestCase):
-    def test_board(self):
-        self.assertTrue(False)
+    def test_board_obvious_clash(self):
+        # Given
+        board = Board(4)
+        s = '1...1...........'
+
+        # When
+        result = board.from_str(s)
+        
+        # Then
+        self.assertFalse(result)
