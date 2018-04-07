b220dd6bb688c45e807b55c9c0fedeb0e94ac15c
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2018-04-07 22:35:29 +0100

    FEAT: Add Operation class
    This allows holding different types of fill-in mechanisms within the `quick_fill` function.

---
+++ operation.py
@@ -0,0 +1,23 @@
+from indexing import boxindex
+
+
+class Operation:
+    def __init__(self, s, i, j):
+        self.finds = s
+        self.indices = (i, j)
+
+    def is_peer_of(self, dig, row, col, box_height, box_width):
+        """
+        Decide if slf is a peer of (dig, row, col), i.e. whether they share a
+        row, column, box or digit.
+        """
+        if self.finds == 'dig':
+            return (row, col) == self.indices
+        elif self.finds == 'row':
+            return (dig, col) == self.indices
+        elif self.finds == 'col':
+            return (dig, row) == self.indices
+        elif self.finds == 'pos':
+            box = boxindex(row, col, box_height, box_width)
+            return (dig, box) == self.indices
+        raise ValueError
