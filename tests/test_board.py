f154e1fa734a2aebc20acd0f27e275647f469940
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2018-04-07 23:14:33 +0100

    TST: Update test_board.py

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,15 +1,54 @@
 import unittest
 
-from ..board import Board
+from board import Board
+
 
 class TestBoard(unittest.TestCase):
-    def test_board_obvious_clash(self):
+
+    def test_board_from_str_obvious_clash(self):
         # Given
         board = Board(4)
         s = '1...1...........'
 
         # When
         result = board.from_str(s)
-        
+
         # Then
         self.assertFalse(result)
+
+    def test_board_from_str_valid_board(self):
+        # Given
+        board = Board(4)
+        s = '1234............'
+
+        # When
+        result = board.from_str(s)
+
+        # Then
+        self.assertTrue(result)
+
+    def test_board_almost_filled(self):
+        # Given
+        board = Board(4)
+        s = '12344321...33412'
+
+        # When
+        board.from_str(s)
+        result = board.solve()
+
+        # Then
+        print(board)
+        self.assertTrue(result)
+
+    def test_board(self):
+        # Given
+        board = Board(4)
+        s = '12344..1.......2'
+
+        # When
+        board.from_str(s)
+        result = board.solve()
+
+        # Then
+        print(board)
+        self.assertTrue(result)
