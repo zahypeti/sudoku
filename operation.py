60e8fbc85a47d97e124bc6e0684f6015af581b59
Peter Zahemszky <pzahemszky@enthought.com>
2018-04-30 00:43:45 +0100

    Add comments about primary peers

--- operation.py
+++ operation.py
@@ -3,6 +3,13 @@ from indexing import boxindex, rows_cols
 
 class Operation:
     def __init__(self, s, i, j):
+        """
+        Should be one of:
+        Operation('dig', row, col)
+        Operation('row', dig, col)
+        Operation('col', dig, row)
+        Operation('pos', dig, box)
+        """
         self.finds = s
         self.indices = (i, j)
 
@@ -34,11 +41,11 @@ class Operation:
             box = boxindex(row, col, box_height, box_width)
             return this_row == row or this_col == col or this_box == box
         elif self.finds == 'row':
-            this_dig = self.indices[0]
+            this_dig = self.indices[0]  # some are primary
             this_col = self.indices[1]
             return this_dig == dig or this_col == col
         elif self.finds == 'col':
-            this_dig = self.indices[0]
+            this_dig = self.indices[0]  # some are primary
             this_row = self.indices[1]
             return this_dig == dig or this_row == row
         elif self.finds == 'pos':
@@ -48,8 +55,8 @@ class Operation:
                                                        box_width)
             box = boxindex(row, col, box_height, box_width)
             row_slice, col_slice = rows_cols(row, col, box_height, box_width)
-            return (this_box == box
-                    or (this_dig == dig and this_row_slice == row_slice)
-                    or (this_dig == dig and this_col_slice == col_slice))
+            return (this_box == box or
+                    (this_dig == dig and this_row_slice == row_slice) or#primry
+                    (this_dig == dig and this_col_slice == col_slice)) #primary
 
         raise ValueError
