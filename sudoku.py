43f8e89c9b685545932f2c645538faec146d6968
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2018-07-21 23:45:05 +0100

    Move __str__(), __repr__(), from_str()

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from math import sqrt
 import string
 
@@ -6,7 +7,8 @@ from solver import Solver
 
 class Sudoku:
     """
-
+    Object that holds the state of a (valid or invalid) sudoku board of a
+    fixed size.
     """
     def __init__(self, *args):
         """
@@ -68,21 +70,95 @@ class Sudoku:
         # Characters used for user-friendly square representation: .,1,2,3 etc
         self._alphabet = '.' + base36digits[1:self._side_length+1]
 
-
         # Non-constant attributes #############################################
 
-        self.board = [[0] * self._side_length] * self._side_length
+        # Nested list of ints containing the 1-based digits (or 0 if empty)
+        self.board = [
+            [0 for _ in range(self._side_length)]
+            for _ in range(self._side_length)]
+
         # Number of empty squares at any point in time
         self.empties = self._side_length ** 2
 
     def __str__(self):
-        pass
+        """
+        User-friendly, readable string description of this Board instance
+        showing the current state of the sudoku board. Uses 1-based indexing
+        for the digits and arranges them in a square. Shows dots (.) for the
+        empty squares.
+        """
+        result = ''
+        for row in self.board:
+            for digit in row:
+                if 0 < digit:
+                    result += str(digit)
+                else:
+                    result += '.'
+            result += '\n'
+
+        return result
 
     def __repr__(self):
-        pass
+        """
+        A short representation of this Board instance showing its size only.
 
-    def from_str(self):
-        pass
+        Use this to create an instance with the initial (empty) state of a
+        sudoku board that has the same size as this.
+        """
+        if self._box_height == self._box_width:
+            return '{}({})'.format(self.__class__.__name__, self._side_length)
+        else:
+            return '{}({}, {})'.format(
+                self.__class__.__name__, self._box_height, self._box_width)
+
+    def from_str(self, characters):
+        """
+        Clear the board and rebuild it from the given character representation.
+
+        Parameters
+        ----------
+        characters : iterable of length-1 strings
+            Single iterable containing the digits (in row-major order) to fill
+            the board with. Digits are interpreted using 1-based indexing,
+            dot (.) and zero (0) represent empty squares. Characters not in
+            _alphabet (or not equal to '0') are ignored.
+
+        Raises
+        ------
+        ValueError
+            If the given character representation does not correspond to a
+            valid sudoku board.
+
+        Examples
+        --------
+        >>> board = Sudoku(4)
+        >>> board.from_str('1234....4321....')
+        """
+
+        # Clear all squares but keep the current size and shape
+        self.__init__(self._box_height, self._box_width)
+
+        # Process characters in parallel with iterating over the squares
+        i = 0
+        char = characters[i]
+        for row in range(self._side_length):
+            for col in range(self._side_length):
+
+                char = characters[i]
+                # Increase the current position within the input argument
+                i += 1
+
+                # Ignore invalid characters
+                while char not in (self._alphabet + '0'):
+                    char = characters[i]
+                    i += 1
+
+                # Dot (.) has the same meaning as zero (0)
+                if char == '.':
+                    char = '0'
+
+                # Use 1-based indexing internally (and 0 for empty squares)
+                self.board[row][col] = int(char, base=36)
 
     def from_file(self):
         pass
