93296fa8e5dcbdb55be85a0343775ef78c76673e
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2018-07-21 20:17:05 +0100

    First version of the Solver class

--- solver.py
+++ solver.py
@@ -1,14 +1,20 @@
-"""
-A custom class for efficiently solving a sudoku board.
-"""
+from copy import deepcopy
+
 import numpy as np
+
+from indexing import rows_cols, square
+from operation import DIGIT_BOX, DIGIT_COLUMN, DIGIT_ROW, ROW_COLUMN
+from operation_queue import OperationQueue
 
 EMPTY_DIGIT = -1
 
 
 class Solver:
+    """
+    A custom class for efficiently solving a sudoku board.
+    """
 
-    def __init__(self, array):
+    def __init__(self, array, box_height, box_width):
         """
         Initialise this instance.
 
@@ -23,3 +29,60 @@ class Solver:
             msg = ('The {} class has to be instantiated with a '
                    'numpy.ndarray.').format(self.__class__.__name__)
             raise TypeError(msg)
+
+        # Constant attributes #################################################
+
+        # The height and width of the box units
+        self._box_height = box_height
+        self._box_width = box_width
+
+        # The side length of the sudoku board
+        self._side_length = self._box_height * self._box_width
+
+        # A single list over the coordinates of the squares in row-major order
+        self._double_loop = [
+            (i, j)
+            for i in range(self._side_length)
+            for j in range(self._side_length)]
+
+        # Non-constant attributes #############################################
+
+        # Main 3D data structure representing the current state of the board
+        self.board = np.array(
+            [[[True]
+              * self._side_length] * self._side_length] * self._side_length,
+            dtype=bool)
+
+        # Solution steps to perform
+        self.operations = OperationQueue(
+            None, self._box_height, self._box_width)
+
+    def secure(self, digit, row, col):
+        pass
+
+    def quick_fill(self):
+        pass
+
+    def _hidden_clash(self):
+        pass
+
+    def _first_empty_square(self):
+        pass
+
+    def recursively_solve(self):
+        """
+        Returns
+        -------
+        result : numpy.ndarray of ndim 2
+            The solved board as a 2D numpy array. Digits are 0-based, and
+            EMPTY_DIGIT items represent empty squares.
+
+        Raises
+        ------
+        RunTimeError
+            If it is found that the input board has no solutions.
+        """
+        pass
+
+    def _update(self):
+        pass
