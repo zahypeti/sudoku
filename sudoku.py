7b725da7668c2e3eb6d14ee6070bcef3c6937b6f
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2018-07-21 22:17:49 +0100

    Copy argument parsing to sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -8,13 +8,56 @@ class Sudoku:
     """
 
     """
-    def __init__(self, side_length):
-        # Parse arguments TODO
+    def __init__(self, *args):
+        """
+        Set up an empty board of a given size. To populate the board, use the
+        from_str() method instead.
+
+        Parameters
+        ----------
+        size : int or 2-tuple of ints
+            If a single argument is given, the board is created with the
+            given argument as its side length. Default is 9.
+            If two arguments are given, the board is created with the given
+            box height and width.
+
+        Raises
+        ------
+        ValueError
+            If more than two arguments are provided.
+            If the side length is not a square number.
+            If the board is too big (side length larger than 36).
+        """
+
+        # Parse arguments #####################################################
+        if len(args) == 0:
+            # Default is a 9x9 board
+            self._box_height = 3
+            self._box_width = 3
+
+        elif len(args) == 1:
+            # Assume a square shaped board with the given side length
+            side_length = args[0]
+            # Raise error if it's not a square number
+            if side_length != int(sqrt(side_length))**2:
+                msg = 'The side length of the board must be a square number.'
+                raise ValueError(msg)
+
+            # Set the box height and width
+            self._box_height = int(sqrt(side_length))
+            self._box_width = int(sqrt(side_length))
+
+        elif len(args) == 2:
+            self._box_height = args[0]
+            self._box_width = args[1]
+
+        else:
+            raise ValueError('Too many arguments.')
 
         # Constant attributes #################################################
 
         # The side length of the sudoku board
-        self._side_length = side_length
+        self._side_length = self._box_height * self._box_width
         # Raise an error if characters would run out due to a large board size
         if 37 <= self._side_length:
             msg = 'The side length of the board must be less than 37.'
