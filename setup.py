f63be935cd50ed7a408503b2f350ac88e3abad8a
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2018-12-17 00:41:41 +0000

    Remove tests.test_api from setup.py test

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setuptools.setup(
     author_email="29452238+pzahemszky@users.noreply.github.com",
     # Requirements installed by `python setup.py install`
     install_requires=["numpy >= 1.14.2"],
-    # Tests run by `python setup.py test`
-    test_suite=["tests.test_api", "tests.test_hb6d_board"],
+    # Test run by `python setup.py test`
+    test_suite="tests.test_hb6d_board",
 )
