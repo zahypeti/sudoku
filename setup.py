031add037912772d8a2de220ea2c67ddf5eed0ba
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2018-12-22 01:55:30 +0000

    Update setup.py (#62)
    * Modify setup.py
    
    * Use README as the long description
    
    * Add Python 3 explicitly

--- setup.py
+++ setup.py
@@ -1,12 +1,51 @@
 import setuptools
 
 
+TROVE_CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Games/Entertainment :: Puzzle Games",
+]
+
+SHORT_DESCRIPTION = "A platform independent sudoku solver written in Python 3."
+
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
 setuptools.setup(
     name="sudoku",
+    version="0.1.0",
+
+    # Short description used by `pip`
+    description=SHORT_DESCRIPTION,
+    # Long description used on PyPI
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url="https://github.com/pzahemszky/sudoku",
     author="Peter Zahemszky",
     author_email="29452238+pzahemszky@users.noreply.github.com",
+
+    # "Trove classifiers" used by PyPI searches
+    classifiers=TROVE_CLASSIFIERS,
+    keywords='sudoku sudoku-solver',
+
+    packages=setuptools.find_packages(exclude=['development', 'tests']),
     # Requirements installed by `python setup.py install`
     install_requires=["numpy >= 1.14.2"],
+    python_requires=">=3.4",
+
+    project_urls={
+        "Tracker": "https://github.com/pzahemszky/sudoku/issues",
+    },
+
     # Test run by `python setup.py test`
     test_suite="sudoku.tests",
 )
