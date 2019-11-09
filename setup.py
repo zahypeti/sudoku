d5827251b48cc7842009d094fa00f9ec16c19a05
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2019-11-09 19:16:19 +0100

    Add Python 3.8 jobs to AppVeyor (#98)
    * Add Python 3.8 jobs to AppVeyor
    
    * Exclude Visual Studio 2013 from Python 3.8 jobs
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ TROVE_CLASSIFIERS = [
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: Implementation :: CPython",
     "Topic :: Games/Entertainment :: Puzzle Games",
 ]
